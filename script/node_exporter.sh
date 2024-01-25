604c21a6324ab01899d7daa403cdf0209aee7641
sergey-arzumanyan <serg.arzumanyan10@gmail.com>
2024-01-25 14:52:22 +0400

    Editing node and prom

--- script/node_exporter.sh
+++ script/node_exporter.sh
@@ -10,7 +10,7 @@ wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_e
 tar xvfz node_exporter-*.tar.gz
 #4. Move the executable to usr/local/bin so it is accessible throughout the system.
 
-sudo mv node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin
+mv node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin
 #5. (Optional) Remove any remaining files.
 
 rm -r node_exporter-1.5.0.linux-amd64*
@@ -19,10 +19,10 @@ rm -r node_exporter-1.5.0.linux-amd64*
 #node_exporter
 #7. It is more convenient to run Node Exporter as a service. To run Node Exporter this way, first, create a node_exporter user.
 
-sudo useradd -rs /bin/false node_exporter
+useradd -rs /bin/false node_exporter
 #8. Create a service file for systemctl to use. The file must be named node_exporter.service and should have the following format. Most of the fields are similar to those found in prometheus.service, as described in the previous section.
 
-sudo mv node_exporter /etc/systemd/system/
+cp node_exporter.service /etc/systemd/system/
 
 #Description=Node Exporter Wants=network-online.target After=network-online.target
 
@@ -31,11 +31,11 @@ sudo mv node_exporter /etc/systemd/system/
 #[Install] WantedBy=multi-user.target
 #9. (Optional) If you intend to monitor the client on an ongoing basis, use the systemctl enable command to automatically launch Node Exporter at boot time. This continually exposes the system metrics on port 9100. If Node Exporter is only intended for occasional use, do not use the command below.
 
-sudo systemctl enable node_exporter
+systemctl enable node_exporter
 
 #10. Reload the systemctl daemon, start Node Exporter, and verify its status. The service should be active.
 
-sudo systemctl daemon-reload sudo systemctl start node_exporter sudo systemctl status node_exporter
+systemctl daemon-reload systemctl start node_exporter systemctl status node_exporter
 
 #Use a web browser to visit port 9100 on the client node, for example, http://local_ip_addr:9100. A page entitled Node Exporter is displayed along with a link reading Metrics. Click the Metrics link and confirm the statistics are being collected. For a detailed explanation of the various statistics, see the Node Exporter Documentation.
 
--- script/prometheus.sh
+++ script/prometheus.sh
@@ -9,51 +9,53 @@
 #--------------------------------------How to Install and Configure Node Exporter on the Client--------------------------------------
 #1. Use wget to download Prometheus to the monitoring server. The target link has the format https://github.com/prometheus/prometheus/releases/download/v[release]/prometheus-[release].linux-amd64.tar.gz. Replace the string [release] with the actual release to download. For example, the following command downloads release 2.37.6.
 
-wget https://github.com/prometheus/prometheus/releases/download/v2.37.6/prometheus-2.37.6.linux-amd64.tar.gz
+#wget https://github.com/prometheus/prometheus/releases/download/v2.37.6/prometheus-2.37.6.linux-amd64.tar.gz
 #2. Extract the archived Prometheus files.
 
-tar xvfz prometheus-*.tar.gz
+#tar xvfz prometheus-*.tar.gz
 #3. (Optional) After the files have been extracted, delete the archive or move it to a different location for storage.
 
-rm prometheus-*.tar.gz
+#rm prometheus-*.tar.gz
 #4. Create two new directories for Prometheus to use. The /etc/prometheus directory stores the Prometheus configuration files. The /var/lib/prometheus directory holds application data.
 
-sudo mkdir /etc/prometheus /var/lib/prometheus
+#mkdir /etc/prometheus /var/lib/prometheus
 #5. Move into the main directory of the extracted prometheus folder. Substitute the name of the actual directory in place of prometheus-2.37.6.linux-amd64.
 
-cd prometheus-2.37.6.linux-amd64
+#cd prometheus-2.37.6.linux-amd64
 #6. Move the prometheus and promtool directories to the /usr/local/bin/ directory. This makes Prometheus accessible to all users.
 
-sudo mv prometheus promtool /usr/local/bin/
+#cp prometheus promtool /usr/local/bin/
 #7. Move the prometheus.yml YAML configuration file to the /etc/prometheus directory.
 
-sudo mv prometheus.yml /etc/prometheus/prometheus.yml
+#cp prometheus.yml /etc/prometheus/prometheus.yml
 #8. The consoles and console_libraries directories contain the resources necessary to create customized consoles. This feature is more advanced and is not covered in this guide. However, these files should also be moved to the etc/prometheus directory in case they are ever required.
 
-sudo mv consoles/ console_libraries/ /etc/prometheus/
+#mv consoles/ console_libraries/ /etc/prometheus/
 #9. Verify that Prometheus is successfully installed using the below command:
 
-prometheus --version
+#prometheus --version
 
 #--------------------------------------How to Configure Prometheus as a Service--------------------------------------
 #1. Create a prometheus user. The following command creates a system user.
 
-sudo useradd -rs /bin/false prometheus
+useradd -rs /bin/false prometheus
 #2. Assign ownership of the two directories created in the previous section to the new prometheus user.
 
-sudo chown -R prometheus: /etc/prometheus /var/lib/prometheus
+chown -R prometheus: /etc/prometheus /var/lib/prometheus
 #3. To allow Prometheus to run as a service, create a prometheus.service file using the following command:
 
-sudo mv prometheus.service /etc/systemd/system/
+cp prometheus.service /etc/systemd/system/
 #Enter the following content into the file:
 
-sudo systemctl daemon-reload
+systemctl daemon-reload
 #5. (Optional) Use systemctl enable to configure the prometheus service to automatically start when the system boots. If this command is not added, Prometheus must be launched manually.
 
-sudo systemctl enable prometheus
+systemctl enable prometheus
 #6. Start the prometheus service and review the status command to ensure it is active.
 
-sudo systemctl start prometheus sudo systemctl status prometheus
+systemctl start prometheus
+
+systemctl status prometheus
 #7. Access the Prometheus web interface and dashboard at http://local_ip_addr:9090. Replace local_ip_addr with the address of the monitoring server. Because Prometheus is using the default configuration file, it does not display much information yet.
 #8. The default prometheus.yml file contains a directive to scrape the local host. Click Status and Targets to list all the targets. Prometheus should display the local Prometheus service as the only target.
 
