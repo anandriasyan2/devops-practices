1b614296d212686307a2909a84fdf400beb15b3c
Anahit Andriasyan <anahitl@rmbg.nl>
2024-01-25 18:43:30 +0400

    updated node and grafana

--- linux-practice/Node_exporter.sh
+++ linux-practice/Node_exporter.sh
@@ -3,17 +3,17 @@
 #Installing node_exporter
 
 ### for x64 arch (common server)
-wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-amd64.tar.gz
+wget https://github.com/prometheus/node_exporter/releases/download/v1.5.0/node_exporter-1.5.0.linux-arm64.tar.gz
 
 #Extract the downloaded archive
 
-tar -xf node_exporter-1.5.0.linux-amd64.tar.gz
-Move the node_exporter binary to /usr/local/bin
+tar -xf node_exporter-1.5.0.linux-arm64.tar.gz
+#Move the node_exporter binary to /usr/local/bin
 
- mv node_exporter-1.5.0.linux-amd64/node_exporter /usr/local/bin
+ mv node_exporter-1.5.0.linux-arm64/node_exporter /usr/local/bin
 #Remove the residual files with:
 
-rm -r node_exporter-1.5.0.linux-amd64*
+rm -r node_exporter-1.5.0.linux-arm64*
 #Next,we will create users and service files for node_exporter.
 
  useradd -rs /bin/false node_exporter
@@ -24,7 +24,7 @@ rm -r node_exporter-1.5.0.linux-amd64*
  systemctl enable node_exporter
  systemctl start node_exporter
  systemctl status node_exporter
-Configure UFW / Firewall Ubuntu :
+#Configure UFW / Firewall Ubuntu :
 
  ufw allow from YOURPROMETHEUSSERVERIP to any port 9100
  ufw status numbered
